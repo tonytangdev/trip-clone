9fb56bd359ac629cb53a7a287e1b2e4827e4721e
tonytangdev <tony.tang.dev@gmail.com>
2021-08-05 22:02:15 +0200

    Header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
+import styles from '../styles/Header.module.css'
+import BurgerMenuButton from './BurgerMenuButton'
+
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles.header}>
+            <div className={styles.headerWrapper}>
+                <div className={styles.headerLogo}>
+                </div>
+            </div>
+            <BurgerMenuButton>
+                <a id="home" className="menu-item" href="/">Home</a>
+                <a id="about" className="menu-item" href="/about">About</a>
+                <a id="contact" className="menu-item" href="/contact">Contact</a>
+            </BurgerMenuButton>
+        </header>
     )
 }
